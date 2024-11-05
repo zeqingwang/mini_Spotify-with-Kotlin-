801de9bab17cef089639f5eba005427def4d1d16
zeqingwang <61963896+zeqingwang@users.noreply.github.com>
2024-11-04 23:59:27 -0800

    Initial

---
+++ app/src/main/java/com/laioffer/spotify/database/DatabaseDao.kt
@@ -0,0 +1,27 @@
+package com.laioffer.spotify.database
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.laioffer.spotify.datamodel.Album
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface DatabaseDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun favoriteAlbum(album: Album)
+
+    @Query("SELECT EXISTS(SELECT * FROM Album WHERE id = :id)")
+    fun isFavoriteAlbum(id : Int) : Flow<Boolean>
+
+    @Delete
+    fun unFavoriteAlbum(album: Album)
+
+
+    @Query("select * from Album")
+    fun fetchFavoriteAlbums(): Flow<List<Album>>
+}
+
